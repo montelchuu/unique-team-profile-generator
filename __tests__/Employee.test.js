a0f1b036497e9a021d598e9bc964468ac80a949d
matthew dehart <mattidehart@hotmail.com>
2022-08-01 12:59:41 -0400

    added engineer

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,8 +3,8 @@ const Employee = require('../lib/Employee.js');
 test('checks for employee name id and email', () => {
     const employee = new Employee('Matt', 12, 'test@test.com');
 
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.name).toBe('Matt');
+    expect(employee.id).toBe(12);
     expect(employee.email).toBe('test@test.com');
 });
 
@@ -12,4 +12,22 @@ test ('checks for getName()', () => {
     const employee = new Employee('Matt', 12, 'test@test.com');
 
     expect(employee.getName()).toBe('Matt');
-})
+});
+
+test ('checks for getId()', () => {
+    const employee = new Employee('Matt', 12, 'test@test.com');
+
+    expect(employee.getId()).toBe(12);
+});
+
+test('checks for getEmail()', () => {
+    const employee = new Employee('Matt', 12, 'test@test.com');
+
+    expect(employee.getEmail()).toBe('test@test.com');
+});
+
+test('checks for getRole()', () => {
+    const employee = new Employee('Matt', 12, 'test@test.com');
+
+    expect(employee.getRole()).toBe('Employee');
+});
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const Engineer = require('../lib/Engineer');
+
+test('checks if Employee extends Engineer', () => {
+    const engineer = new Engineer('Matt', 12, 'test@test.com', 'montelchuu');
+
+    expect(engineer.name).toBe('Matt');
+    expect(engineer.id).toBe(12);
+    expect(engineer.email).toBe('test@test.com');
+    expect(engineer.github).toBe('montelchuu')
+})
+
+test('checks for Engineer getGithub()', () => {
+    const engineer = new Engineer('Matt', 12, 'test@test.com', 'montelchuu');
+
+    expect(engineer.getGithub()).toBe('montelchuu');
+})
+
+test('checks for Engineer getRole', () => {
+    const engineer = new Engineer('Matt', 12, 'test@test.com', 'montelchuu');
+
+    expect(engineer.getRole()).toBe('Engineer');
+})
