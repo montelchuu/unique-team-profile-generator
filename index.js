73527c58e2ad33bb523403616af80c807e2bcace
matthew dehart <mattidehart@hotmail.com>
2022-08-10 15:24:17 -0400

    adding a lot because internet was out for a week, sorry for not a lot of commits

--- index.js
+++ index.js
@@ -1,31 +1,222 @@
-// AS A manager
-// I WANT to generate a webpage that displays my team's basic info
-// SO THAT I have quick access to their emails and GitHub profiles
+// Dependencies
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
 
+// Constructors
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+const render = require('./src/page-template.js');
 
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
+// Empty arrays for employees
+const employees = [];
 
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
+// Add manager option
+const addManager = () => {
+    console.log("---------------");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Please enter the team manager's name",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    }
+                    return "Please enter a name";
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter the team manager's ID number",
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    }
+                    return "Please enter the ID number";
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the team manager's email",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    }
+                    return "Please enter an email";
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "Please enter the team manager's office number",
+                validate: officeNumberInput => {
+                    if (officeNumberInput) {
+                        return true;
+                    }
+                    return "Please enter the office number";
+                }
+            }
+        ])
+        .then(data => {
+            const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+           
+            employees.push(manager);
+            addTeam();
+        })
+}
 
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// After the manager has been completed, the choice to add team members is given
+const addTeam = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employeeOption',
+                message: "Please choose the next team member's position or finish building your team",
+                choices: ['Engineer', 'Intern', 'Finish']
+            }
+        ])
+        .then(data => {
+            if (data.employeeOption === 'Engineer') {
+                addEngineer();
+            } else if (data.employeeOption === 'Intern') {
+                addIntern();
+            } else {
+                generatehtml();
+            }
+        })
+}
 
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// Add engineer option
+const addEngineer = () => {
+    console.log("---------------");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                messge: "Please enter the engineer's name",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    }
+                    return "Please enter a name"
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter the engineer's ID number",
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    }
+                    return "Please enter the ID number"
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the engineer's email",
+                validate: emailInput => {
+                    if(emailInput) {
+                        return true;
+                    }
+                    return "Please enter a valid email"
+                }
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Please enter the engineer's GitHub username",
+                validate: githubInput => {
+                    if(githubInput) {
+                        return true;
+                    }
+                    return "Please enter the GitHub username"
+                }
+            }
+        ])
+        .then(data => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+           
+            employees.push(engineer);
+            addTeam();
+        })
+}
 
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// Add intern option
+const addIntern = () => {
+    console.log("---------------");
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Please enter the intern's name",
+                validate: nameInput => {
+                    if (nameInput) {
+                      return true;
+                    }
+                    return "Please enter a name";
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter the intern's ID number",
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    }
+                    return "Please enter the ID number"
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the intern's email",
+                validate: emailInput => {
+                    if(emailInput) {
+                        return true;
+                    }
+                    return "Please enter a valid email"
+                }
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Please enter the intern's school name",
+                validate: schoolInput => {
+                    if(schoolInput) {
+                        return true;
+                    }
+                    return "Please enter the school name"
+                }
+            }
+        ])
+        .then(data => {
+            const intern = new Intern(data.name, data.id, data.email, data.school);
 
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+            employees.push(intern);
+            addTeam();
+        })
+}
 
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+// Create html after collecting data
+function generatehtml() {
+    fs.writeFileSync(outputPath, render(employees), "utf-8");
+    console.log('Successfully created index.html')
+    process.exit(0);
+}
 
+addManager();
